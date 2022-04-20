396c7aa34501ba007c23da29be8a28e24a46774e
Viktor <54067422+ChepelVE@users.noreply.github.com>
2022-04-20 20:04:13 +0300

    user registration added

--- coolsite/women/utils.py
+++ coolsite/women/utils.py
@@ -5,8 +5,7 @@ from women.models import *
 
 menu = [{'title': "О сайте", 'url_name': "about"},
         {'title': "Добавить статью", 'url_name': "add_page"},
-        {'title': "Обратная связь", 'url_name': "contact"},
-        {'title': "Войти", 'url_name': "login"}]
+        {'title': "Обратная связь", 'url_name': "contact"}]
 
 
 class DataMixin:
--- coolsite/women/views.py
+++ coolsite/women/views.py
@@ -13,8 +13,7 @@ from .utils import *
 menu = [{'title': "О сайте", 'url_name': "about"},
         {'title': "Добавить статью", 'url_name': "add_page"},
         {'title': "Обратная связь", 'url_name': "contact"},
-        {'title': "Войти", 'url_name': "login"}
-        ]
+        {'title': "Войти", 'url_name': "login"}]
 
 
 class WomenHome(DataMixin, ListView):
@@ -154,3 +153,15 @@ class WomenCategory(DataMixin, ListView):
 
 def page_not_found(request, exception):
     return HttpResponseNotFound('<h1>Страница не найдена</h1>')
+
+
+class RegisterUser(DataMixin, CreateView):
+    form_class = RegisterUserForm
+    template_name = 'women/register.html'
+    success_url = reverse_lazy('login')
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Регистрация')
+        return dict(list(context.items()) + list(c_def.items()))
+        return context
